ea28d1fc842a02858a4bac5f868bf322ea8a7ea1
Mat Evans <matzhouse@gmail.com>
2015-11-30 10:49:00 +0000

    add some new elements

--- gcfilter.go
+++ gcfilter.go
@@ -4,18 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"runtime"
 
 	syscall "golang.org/x/sys/unix"
 )
 
 var current *os.File
 
-type Wall struct {
+// GCRun holds data about a GC run
+type GCRun struct {
+	N     int     // the GC number, incremented at each GC
+	Time  float32 //time in seconds since program start
+	Perc  int     // percentage of time spent in GC since program start
+	Clock Timing
+	CPU   Timing
+	Heap  Heap
+	Procs int
+}
+
+// Timing is the base struct for clock or cpu timing information
+type Timing struct {
 	SweepTerm, Scan, Sync, Mark, MarkTerm float64
+}
+
+// Heap tracks the size of the heap during a GC run
+type Heap struct {
+	start int
+	end   int
+	live  int
+	goal  int
 }
 
 // Filter ..
 func Filter() error {
+
+	if GCOn() == false {
+		return fmt.Errorf("GC Debug flag not set")
+	}
 
 	fmt.Println("filtering GC statements")
 
@@ -58,7 +83,7 @@ func Filter() error {
 			var number int
 
 			if line[0:2] == "gc" {
-				w := &Wall{}
+				run := &GCRun{}
 				// gc 1 @0.248s 0%: 0.14+0.43+0.33+0.54+0.43 ms clock, 0.29+0.43+0+0.13/0.44/1.4+0.86 ms cpu, 4->4->1 MB, 4 MB goal, 4 P
 				fmt.Sscanf(line, "gc %d %s %s %s ms clock, %s ms cpu,", &number, &time, &perc, &wall, &cpu)
 				fmt.Sscanf(wall, "%f+%f+%f+%f+%f", &w.SweepTerm, &w.Scan, &w.Sync, &w.Mark, &w.MarkTerm)
@@ -71,6 +96,13 @@ func Filter() error {
 	}()
 
 	return nil
+}
+
+func GCOn() bool {
+	var ms *runtime.MemStats
+	runtime.ReadMemStats(ms)
+
+	return ms.DebugGC
 }
 
 // Reset returns Stderr to the original location
